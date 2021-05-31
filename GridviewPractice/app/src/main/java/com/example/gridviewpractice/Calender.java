100854bbfb6f3f2585a8f30aaf77a0d9c44fff07
inter19999 <inter19999@naver.com>
2021-06-01 00:42:36 +0900

    그리드 뷰

---
+++ GridviewPractice/app/src/main/java/com/example/gridviewpractice/Calender.java
@@ -0,0 +1,5 @@
+package com.example.gridviewpractice;
+
+public class Calender {
+
+}
--- GridviewPractice/app/src/main/java/com/example/gridviewpractice/MainActivity.java
+++ GridviewPractice/app/src/main/java/com/example/gridviewpractice/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.gridviewpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Gridview");
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int LastDays = cal.getActualMaximum(Calendar.DATE);
 
-        String[] items = {"1","2","3","4","5","6","7","8","9","10","11","12","13",
-                "14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"
-        ,"31","32","33","34","35","36","37","38","39","40","41","42"};
-        /*String[] items = new String[42];
-        for(int i=0;i<42;i++){
-            items[i]=" ";
-        }*/
+        getSupportActionBar().setTitle(year + "년" + month + "월");
+
+        String[] items = new String[42];
+        for (int i = 0; i < 42; i++) {
+            items[i] = "" + (i + 1);
+        }
+
+
 
         //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
         ArrayAdapter<String> adapt
@@ -33,21 +43,35 @@ public class MainActivity extends AppCompatActivity {
         GridView gridview = (GridView) findViewById(R.id.gridview);
         // 어댑터를 GridView 객체에 연결
         gridview.setAdapter(adapt);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(),year+"."+month+"."+ (position+1),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
+
+    private static int getLastDay(int month) {
+        if(month == 4 || month == 6 || month == 9 || month == 11){
+            return 30;
+        } else if (month == 2) {
+            return 28;
+        } else {
+            return 31;
+        }
+
+    }
+    public void processDatePickerResult(int year, int month, int day){
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this,"Date: "+dateMessage, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 }
