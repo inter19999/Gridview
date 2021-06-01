b59704bbd0824139f10ad4b6768b5aadc804c97e
inter19999 <83069555+inter19999@users.noreply.github.com>
2021-06-01 18:54:33 +0900

    그리드뷰
    그리드 뷰

--- GridviewPractice/app/src/main/java/com/example/gridviewpractice/MainActivity.java
+++ GridviewPractice/app/src/main/java/com/example/gridviewpractice/MainActivity.java
@@ -20,17 +20,23 @@ public class MainActivity extends AppCompatActivity {
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
         int LastDays = cal.getActualMaximum(Calendar.DATE);
-
-        getSupportActionBar().setTitle(year + "년" + month + "월");
+        int dayofweek = cal.get(Calendar.DAY_OF_WEEK);
+        int j=0;
+        Calendar firstmonth = Calendar.getInstance();
+        firstmonth.set(year,month,1);
+        getSupportActionBar().setTitle(year + "년" + (month+1) + "월");
 
         String[] items = new String[42];
-        for (int i = 0; i < 42; i++) {
-            items[i] = "" + (i + 1);
+        int dow = (firstmonth.get(Calendar.DAY_OF_WEEK)-1);
+        for (int i=0; i<42;i++) {
+            items[i] = "";
         }
-
-
+        int lastDate = firstmonth.getActualMaximum(Calendar.DATE);
+        for (int i =(dow); i<lastDate+dow; i++) {
+            j++;
+            items[i]=""+j;
+        }
 
         //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
         ArrayAdapter<String> adapt
@@ -45,31 +51,13 @@ public class MainActivity extends AppCompatActivity {
         gridview.setAdapter(adapt);
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(getApplicationContext(),year+"."+month+"."+ (position+1),
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),year+"."+(month+1)+"."+ (position-1),
+                        Toast.LENGTH_SHORT).show();// 토스트 메세지 출력 다시 만들어야 함
             }
         });
 
     }
-
-    private static int getLastDay(int month) {
-        if(month == 4 || month == 6 || month == 9 || month == 11){
-            return 30;
-        } else if (month == 2) {
-            return 28;
-        } else {
-            return 31;
-        }
-
-    }
-    public void processDatePickerResult(int year, int month, int day){
-        String month_string = Integer.toString(month+1);
-        String day_string = Integer.toString(day);
-        String year_string = Integer.toString(year);
-        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
-
-        Toast.makeText(this,"Date: "+dateMessage, Toast.LENGTH_SHORT).show();
-    }
+// 달력 부분 프래그넌트로 만들기
 
 
 
